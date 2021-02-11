14c93a17e0a9755ed5fc86f0cc3bc6d2c3c31a75
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-02-11 21:22:25 +0200

    1-7 done

--- index.php
+++ index.php
@@ -39,13 +39,13 @@ echo 'Didesnių už "150" skaičių yra: ' . $count;
 
 $randomLength = rand(3000, 4000);
 $array = [];
-for ($i=1; $i < $randomLength; $i++) { 
+for ($i = 1; $i < $randomLength; $i++) {
     if ($i % 77 === 0) {
         $lastNumber = $i;
         array_push($array, $lastNumber . ', ');
     }
 }
-array_splice($array, count($array)-1);
+array_splice($array, count($array) - 1);
 foreach ($array as $value) {
     echo $value;
 }
@@ -56,12 +56,12 @@ echo $lastNumber;
 
 <?php
 
-echo 'Kvadratas';
+echo 'Kvadratas 25x25';
 echo '<br><br>';
 $length = 25;
 for ($i = 0; $i < $length; $i++) {
     for ($j = 0; $j < $length; $j++) {
-        echo "<span style='display: inline-block; height: 5px; width: 5px;'>*</span>";                
+        echo "<span style='display: inline-block; height: 5px; width: 5px;'>*</span>";
     }
     echo '<br>';
 }
@@ -71,7 +71,107 @@ for ($i = 0; $i < $length; $i++) {
 
 <?php
 
+echo 'Kvadratas 25x25 su raudonom įstrižainėm';
+echo '<br><br>';
+$length = 25;
+for ($i = 0; $i < $length; $i++) {
+    for ($j = 0; $j < $length; $j++) {
+        if ($j === $i) {
+            echo "<span style='display: inline-block; height: 5px; width: 5px; color: red;'>*</span>";
+        } else if ($j === ($length - 1 - $i)) {
+            echo "<span style='display: inline-block; height: 5px; width: 5px; color: red;'>*</span>";
+        } else {
+            echo "<span style='display: inline-block; height: 5px; width: 5px;'>*</span>";
+        }
+    }
+    echo '<br>';
+}
+?>
 
+<h2>ND nr.6</h2>
+
+<?php
+
+echo 'Monetos metimą stabdome iškritus herbui:';
+echo '<br>';
+do {
+    $metimas = rand(0, 1);
+    ($metimas === 0) ? print 'H' : print 'S';
+    if ($metimas === 0) {
+        break;
+    }
+} while (true);
+echo '<br>';
+echo 'Monetos metimą stabdome iškritus skaičiui:';
+echo '<br>';
+do {
+    $metimas = rand(0, 1);
+    ($metimas === 0) ? print 'H' : print 'S';
+    if ($metimas === 1) {
+        break;
+    }
+} while (true);
+echo '<br>';
+echo 'Monetos metimą stabdome 3 kartus iškritus herbui:';
+echo '<br>';
+$count = 0;
+do {
+    $metimas = rand(0, 1);
+    ($metimas === 0) ? print 'H' : print 'S';
+    if ($metimas === 0) {
+        $count++;
+    }
+    if ($count === 3) {
+        break;
+    }
+} while (true);
+echo '<br>';
+echo 'Monetos metimą stabdome 3 kartus iš eilės iškritus herbui:';
+echo '<br>';
+$count = 0;
+do {
+    $metimas = rand(0, 1);
+    ($metimas === 0) ? print 'H' : print 'S';
+    if ($metimas === 0) {
+        $count++;
+    } else {
+        $count = 0;
+    }
+    if ($count === 3) {
+        break;
+    }
+} while (true);
+?>
+
+<h2>ND nr.7</h2>
+
+<?php
+
+$petroCount = 0;
+$kazioCount = 0;
+do {
+$petroTaskai = rand(10, 20);
+$kazioTaskai = rand(5, 25);
+if ($petroTaskai > $kazioTaskai) {
+    $laimetojas = 'Petras';
+} else if ($petroTaskai < $kazioTaskai) {
+    $laimetojas = 'Kazys';
+} else {
+    $laimetojas = 'Kazys ir Petras surinko po lygiai taškų';
+}
+echo "Petras taškai: $petroTaskai, Kazio taškai: $kazioTaskai. Partiją laimėjo: ​$laimetojas.";
+$petroCount += $petroTaskai;
+$kazioCount += $kazioTaskai;
+echo '<br>';
+} while ($petroCount < 222 || $kazioCount < 222);
+$zaidimoLaimetojas = ($petroCount > $kazioCount) ? "Petras surinkęs $petroCount" : "Kazys surinkęs $kazioCount";
+echo '<br>';
+echo "Žaidimą laimėjo ​$zaidimoLaimetojas.";
+?>
+
+<h2>ND nr.8</h2>
+
+<?php
 
 
 
